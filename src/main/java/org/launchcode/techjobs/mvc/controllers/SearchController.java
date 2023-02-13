097081ebb31e6008b56083b7d43e057aadf8dd1d
Robert Bolkan <robert.bolkan@yahoo.com>
2023-02-13 08:33:59 -0800

    initial and for each loop list-jobs.html

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -5,6 +5,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
 
@@ -15,12 +16,13 @@ import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoic
 @RequestMapping("search")
 public class SearchController {
 
-    @GetMapping(value = "")
-    public String search(Model model) {
-        model.addAttribute("columns", columnChoices);
-        return "search";
-    }
+        @GetMapping(value = "")
+        public String search(Model model) {
+            model.addAttribute("columns", columnChoices);
+            return "search";
+        }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+        // TODO #3 - Create a handler to process a search request and render the updated search view.
+
 
 }
